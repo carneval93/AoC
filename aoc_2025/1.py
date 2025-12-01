7487c06211b5e7fa1dcdc273c68776ee73af32c8
Anthony Carnevale (DE-300) <anthony.carnevale@bmw.de>
2025-12-01 08:57:28 +0100

    add day 1

---
+++ aoc_2025/1.py
@@ -0,0 +1,27 @@
+from collections import deque
+inp = open("../input.txt").read().splitlines()
+
+q_list = deque(range(100))
+
+q_list.rotate(50)
+p1 = 0
+p2 = 0
+for inst in inp:
+    before = q_list[0]
+    if 'R' in inst:
+        step = int(inst.split('R')[1])
+        q_list.rotate(-1*step)
+        zero_rotations = step // len(q_list)
+    else:
+        step = int(inst.split('L')[1])
+        q_list.rotate(step)
+        zero_rotations = step // len(q_list)
+    after = q_list[0]
+    p2 += zero_rotations
+    if after == 0:
+        p1 += 1
+    if after == 0 or 'R' in inst and after < before != 0 or 'L' in inst and after > before != 0:
+        p2 += 1
+
+print(p1)
+print(p2)
